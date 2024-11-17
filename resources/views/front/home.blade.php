fe595099efe4f6559734ff003f288c3d5611344f
masum Hoque <turfamartsss@gmail.com>
2024-11-17 19:27:57 +0600

    dedwdw

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -47,54 +47,16 @@
     <div class="container">
         <h2>Popular Categories</h2>
         <div class="row pt-5">
-            <div class="col-lg-4 col-xl-3 col-md-6">
-                <div class="single_catagory">
-                    <a href="jobs.html"><h4 class="pb-2">Design &amp; Creative</h4></a>
-                    <p class="mb-0"> <span>50</span> Available position</p>
+            @if($categories->isNotEmpty())
+                @foreach ($categories as $category)
+                <div class="col-lg-4 col-xl-3 col-md-6">
+                    <div class="single_catagory">
+                        <a href="jobs.html"><h4 class="pb-2">{{ $category->name }}</h4></a>
+                        <p class="mb-0"> <span>50</span> Available position</p>
+                    </div>
                 </div>
-            </div>
-            <div class="col-lg-4 col-xl-3 col-md-6">
-                <div class="single_catagory">
-                    <a href="jobs.html"><h4 class="pb-2">Finance</h4></a>
-                    <p class="mb-0"> <span>50</span> Available position</p>
-                </div>
-            </div>
-            <div class="col-lg-4 col-xl-3 col-md-6">
-                <div class="single_catagory">
-                    <a href="jobs.html"><h4 class="pb-2">Banking</h4></a>
-                    <p class="mb-0"> <span>50</span> Available position</p>
-                </div>
-            </div>
-            <div class="col-lg-4 col-xl-3 col-md-6">
-                <div class="single_catagory">
-                    <a href="jobs.html"><h4 class="pb-2">Data Science</h4></a>
-                    <p class="mb-0"> <span>50</span> Available position</p>
-                </div>
-            </div>
-            <div class="col-lg-4 col-xl-3 col-md-6">
-                <div class="single_catagory">
-                    <a href="jobs.html"><h4 class="pb-2">Marketing</h4></a>
-                    <p class="mb-0"> <span>50</span> Available position</p>
-                </div>
-            </div>
-            <div class="col-lg-4 col-xl-3 col-md-6">
-                <div class="single_catagory">
-                    <a href="jobs.html"><h4 class="pb-2">Digital Marketing</h4></a>
-                    <p class="mb-0"> <span>50</span> Available position</p>
-                </div>
-            </div>
-            <div class="col-lg-4 col-xl-3 col-md-6">
-                <div class="single_catagory">
-                    <a href="jobs.html"><h4 class="pb-2">Digital Marketing</h4></a>
-                    <p class="mb-0"> <span>50</span> Available position</p>
-                </div>
-            </div>
-            <div class="col-lg-4 col-xl-3 col-md-6">
-                <div class="single_catagory">
-                    <a href="jobs.html"><h4 class="pb-2">Digital Marketing</h4></a>
-                    <p class="mb-0"> <span>50</span> Available position</p>
-                </div>
-            </div>
+                @endforeach
+           @endif
         </div>
     </div>
 </section>
